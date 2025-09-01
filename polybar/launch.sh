e8a007e9042fb178be55fe86143367ae36370ec0
Liyanxyz <liyan.ge.de@gmail.com>
2025-09-01 19:45:08 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar top &
+polybar monitor1 &
+polybar monitor2 &
